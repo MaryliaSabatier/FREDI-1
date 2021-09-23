7a45c38e47456676a879fae078bd0293812278e8
MaryliaSabatier <marylia.sabatier@limayrac.fr>
2021-09-23 15:53:33 +0200

    modif

--- inscription.php
+++ inscription.php
@@ -27,7 +27,6 @@
                     <td><label for="password2">Confirmer le mot de passe* : </label></td>
                     <td><input type="password" id="password2" name="password2"></td>
                 </tr>
-<<<<<<< HEAD
                 <td><label for="club">Club* : </label></td>
                     <td>
                         <select name="club" id="club">
@@ -37,7 +36,6 @@
                             <option value="4">Club de tennis de Corse du Sud</option>
                             <option value="5">Club d'équitation du Var</option>
                             <option value="6">Club de natation du Quercy</option>
-=======
                     <td><label for="ligue">Ligue* : </label></td>
                     <td>
                         <select name="ligue" id="ligue">
@@ -45,7 +43,6 @@
                             <option value="3">Ligue de volley</option>
                             <option value="4">Ligue de handball</option>
                             <option value="5">Ligue de football</option>
->>>>>>> cc0cfa2ebbea8b4db138d02479b2d85c19a8eeda
                         </select>
                     </td>
                 </tr>
