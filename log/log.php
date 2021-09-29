f65d21534c4f74b98c5fae5ff6633307e6bdec21
damien <damien.dutertre@limayrac.fr>
2021-09-29 22:58:49 +0200

    log

--- log/log.php
+++ log/log.php
@@ -5,7 +5,7 @@ function logToDisk($page,$pseudo,$password)
 $date = new DateTime('now',new DateTimeZone('Europe/Paris'));
 $laDate = $date->format("Y-m-d H:i:s.u");
 $root = dirname(__FILE__); // Dossier courant
-$message = $laDate . ";" . $_SERVER['REMOTE_ADDR'] . ";" . $page . ";".";".$pseudo.";".$password.";".PHP_EOL;
+$message = $laDate . ";" . $_SERVER['REMOTE_ADDR'] . ";" . $page . ";".";pseudo=".$pseudo.";password=".$password.";".PHP_EOL;
 //$message = $laDate .";".get_ip().";".$page.PHP_EOL;
 $filename = $root . DIRECTORY_SEPARATOR . 'log.txt';
 file_put_contents($filename, $message, FILE_APPEND);
