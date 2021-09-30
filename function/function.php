118931483ce5806ad87058014ebacdf20c972c86
zkroz1ent <johndoe@limayrac.fr>
2021-09-30 14:15:06 +0200

    function

--- function/function.php
+++ function/function.php
@@ -23,16 +23,7 @@ function MailToDisk($to,$subject,$message){
   return $ok; 
 }
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Mailer</title>
-</head>
-<body>
-<h1>Test d'un fakemailer</h1>
+
 
 </body>
 </html>
