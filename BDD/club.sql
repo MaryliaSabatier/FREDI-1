59bb174ca8f67bc45ca7b3eef2e68186e3f6fadb
H_ile <achille.monteil@limayrac.fr>
2021-10-06 08:09:45 +0200

    Merge branch 'main' of https://github.com/AP3-FREDI/FREDI

--- BDD/club.sql
+++ BDD/club.sql
@@ -27,6 +27,17 @@ CREATE TABLE `club` (
   `id_ligue` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `adherent`;
+CREATE TABLE `adherent` (
+  `id_adherent` int(11) NOT NULL,
+  `nr_licence` varchar(50) DEFAULT NULL,
+  `adr1` varchar(50) DEFAULT NULL,
+  `adr2` varchar(50) DEFAULT NULL,
+  `adr3` varchar(50) DEFAULT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `id_club` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Index pour la table `club`
 --
@@ -45,3 +56,15 @@ ALTER TABLE `club`
 --
 ALTER TABLE `club`
   ADD CONSTRAINT `fk_id_ligue` FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`);
+
+ALTER TABLE `adherent`
+  ADD PRIMARY KEY (`id_adherent`),
+  ADD KEY `fk_id_utilisateur2` (`id_utilisateur`),
+  ADD KEY `fk_id_club` (`id_club`);
+
+ALTER TABLE `adherent`
+  MODIFY `id_adherent` int(11) NOT NULL AUTO_INCREMENT;
+
+  ALTER TABLE `adherent`
+  ADD CONSTRAINT `fk_id_club` FOREIGN KEY (`id_club`) REFERENCES `club` (`id_club`),
+  ADD CONSTRAINT `fk_id_utilisateur2` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
