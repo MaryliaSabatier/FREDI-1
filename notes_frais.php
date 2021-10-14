d43c4dffbd12c232b41cafc051b31e77bc439947
MaryliaSabatier <marylia.sabatier@limayrac.fr>
2021-10-14 16:19:08 +0200

    ajout + modif

--- notes_frais.php
+++ notes_frais.php
@@ -18,31 +18,38 @@ include 'header.php';
 <h1>Affichage de la note de frais</h1>
     <img src="img/logo.png">
     <?php
-    echo'<h1> Je sousigné()e'.$session['user']['nom'].$session['prenom']</h1>';
-    
+     echo'<p>Je sousigné()e'.$session['user']['nom'].$session['prenom'].'</p>';
+     echo'<p>Demeurant'.$session['user']['adr1'].$session['adr2'].$session['adr3'].'</p>';
+     echo'<p>Certifie renoncer au remboursement des frais ci*dessous et les laisser à l association (mettre le nom + adresse) en tant que don </p>';
     ?>
+    <h3>Frais de déplacement</h3>
         <table class="container">
         <tr>
-          <th>Id utilisateur</th>
-          <th>Pseudo</th>
-          <th>Mot de passe</th>
-          <th>Mail</th>
-          <th>Nom</th>
-          <th>Prénom</th>
-          <th>Rôle</th>
+          <th>Date</th>
+          <th>Motif</th>
+          <th>Trajet</th>
+          <th>Kms parcourus</th>
+          <th>Total frais kms</th>
+          <th>Cout péages</th>
+          <th>Cout repas</th>
+          <th>Cout hébergement</th>
+          <th>Total</th>
           <th>&nbsp;</th>
         </tr>
         <?php
         foreach ($rows as $row) {
           echo '<tr>';
-          echo '<td>' . $row['id_utilisateur'] . '</td>';
-          echo '<td>' . $row['pseudo'] . '</td>';
-          echo '<td>' . $row['mdp'] . '</td>';
-          echo '<td>' . $row['mail'] . '</td>';
-          echo '<td>' . $row['nom'] . '</td>';
-          echo '<td>' . $row['prenom'] . '</td>';
-          echo '<td>' . $row['role'] . '</td>';
-          echo '<td><a href="modif_utilisateur.php?id_utilisateur=' . $row['id_utilisateur'] . '">Modifier</a>&nbsp;';
+          echo '<td>' . $row['dat_ligne'] . '</td>';
+          echo '<td>' . $row['id_motif'] . '</td>';
+          echo '<td>' . $row['lib_trajet'] . '</td>';
+          echo '<td>' . $row['nb_km'] . '</td>';
+          echo '<td>' . $row['mt_km'] . '</td>';
+          echo '<td>' . $row['mt_peage'] . '</td>';
+          echo '<td>' . $row['mt_repas'] . '</td>';
+          echo '<td>' . $row['mt_hebergement'] . '</td>';
+          echo '<td>' . $row['mt_total'] . '</td>';
+          echo '<td><a href="modif_notes.php?id_ligne=' . $row['id_ligne'] . '">Modifier</a>&nbsp;';
+          echo '<td><a href="supprimer_notes.php?id_ligne=' . $row['id_ligne'] . '">Supprimer</a>&nbsp;';
           echo "</tr>";
         }?>
       </table>
