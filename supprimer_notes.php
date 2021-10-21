5437d7261c38f2f26b250605d34929030edd2652
MaryliaSabatier <marylia.sabatier@limayrac.fr>
2021-10-21 14:27:57 +0200

    modif

--- supprimer_notes.php
+++ supprimer_notes.php
@@ -73,7 +73,7 @@ if ($submit) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ph36 - Marvel</title>
+  <title>FREDI</title>
   <link rel="stylesheet" href="css/styles.css">
 </head>
 
