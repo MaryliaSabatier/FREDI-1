6a233b736e8e311d34f7827029b89026b53a6e91
H_ile <achille.monteil@limayrac.fr>
2021-11-18 14:14:49 +0100

    chenille

--- BDD/triggers/Ligne_de_frais_insert.sql
+++ BDD/triggers/Ligne_de_frais_insert.sql
@@ -4,5 +4,6 @@ ON ligne FOR EACH ROW
 BEGIN
 
 SET new.mt_total = new.nb_km * new.mt_km + new.mt_peage + new.mt_repas + new.mt_hebergement;
+update  note
 SET mt_total = new.mt_total where id_note = new.id_note
 END 
--- BDD/triggers/ligne_de_frais_update.sql
+++ BDD/triggers/ligne_de_frais_update.sql
@@ -4,6 +4,6 @@ ON ligne FOR EACH ROW
 BEGIN
 
 SET new.mt_total = new.nb_km * new.mt_km + new.mt_peage + new.mt_repas + new.mt_hebergement;
-update ON note 
+update  note 
 SET mt_total = new.mt_total where id_note = new.id_note
 END 
