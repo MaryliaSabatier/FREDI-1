e0eedb6cd87fd6d66b4bb7f9fe5b45949772ff22
H_ile <achille.monteil@limayrac.fr>
2021-11-18 14:16:37 +0100

    morah oti motar

--- BDD/triggers/Ligne_de_frais_insert.sql
+++ BDD/triggers/Ligne_de_frais_insert.sql
@@ -5,5 +5,5 @@ BEGIN
 
 SET new.mt_total = new.nb_km * new.mt_km + new.mt_peage + new.mt_repas + new.mt_hebergement;
 update  note
-SET mt_total = new.mt_total where id_note = new.id_note
-END 
+SET mt_total = new.mt_total where id_note = new.id_note ;
+END |
