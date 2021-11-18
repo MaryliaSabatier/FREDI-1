9afdb05070317e9c2c52bdef8efba7131e0c0539
H_ile <achille.monteil@limayrac.fr>
2021-11-18 14:03:21 +0100

    vérifions ensemble

--- BDD/triggers/Ligne_de_frais_insert.sql
+++ BDD/triggers/Ligne_de_frais_insert.sql
@@ -1,8 +1,8 @@
 DELIMITER |
-CREATE OR REPLACE TRIGGER Ligne_de_frais_insert after INSERT
+CREATE OR REPLACE TRIGGER Ligne_de_frais_insert before INSERT
 ON ligne FOR EACH ROW
 BEGIN
 
-SET new.mt_total = new.mb_km * new.mt_km + new.mt_peage + new.mt_repas + new.mt_hebergement;
+SET new.mt_total = new.nb_km * new.mt_km + new.mt_peage + new.mt_repas + new.mt_hebergement;
 
 END 
